d9d941c5a39d674bf46a983e4004452a2556a721
Ben Creasy <ben@bencreasy.com>
2025-08-17 23:13:02 -0700

    checkpoint - fix grep line buffering

--- run-woeusbgui.sh
+++ run-woeusbgui.sh
@@ -23,7 +23,7 @@ sudo env \
   XDG_CACHE_HOME="$XDG_CACHE_HOME" \
   GTK_IM_MODULE_FILE="$GTK_IM_MODULE_FILE" \
   WXSUPPRESS_SIZER_FLAGS_CHECK="$WXSUPPRESS_SIZER_FLAGS_CHECK" \
-  $(which woeusbgui)
+  $(which woeusbgui) 2>&1 | grep --line-buffered -v "gtk_widget_hide: assertion 'GTK_IS_WIDGET (widget)' failed" | sed -u '/^[[:space:]]*$/d'
   # Temporarily disable filtering to see installation progress
   # | grep -v -E "(gtk_widget_hide: assertion|Unable to load resource for composite template|_gtk_css_provider_load_named: assertion|gtk_widget_class_bind_template|gtk_container_|gtk_box_|gtk_button_box_|gtk_label_|gtk_widget_init_template|gtk_widget_get_parent|gtk_widget_set_)" | grep -v "^\s*$"
   # G_MESSAGES_DEBUG="" \
